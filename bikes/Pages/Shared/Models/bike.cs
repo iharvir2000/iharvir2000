ab4a6079e0055e566ec8684164af11d732689fe4
Harvir Singh <iharvir2000@gmail.com>
2023-06-23 16:04:04 -0400

    Add validation

--- bikes/Pages/Shared/Models/bike.cs
+++ bikes/Pages/Shared/Models/bike.cs
@@ -8,18 +8,38 @@ namespace bikes.Models
     public class bike
     {
         public int ID { get; set; }
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
         public string Model { get; set; }
 
         [Display(Name = "Release Date")]
         [DataType(DataType.Date)]
         public DateTime BuildTime { get; set; }
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
+        [Required]
+        [StringLength(30)]
         public string Place { get; set; }
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
+        [Required]
+        [StringLength(30)]
         public string Company { get; set; }
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
+        [Required]
+        [StringLength(30)]
         public string Power { get; set; }
 
+        [Range(1, 100)]
+        [DataType(DataType.Currency)]
         [Column(TypeName = "decimal(18, 2)")]
+        [Required]
         public decimal Cost { get; set; }
+        [RegularExpression(@"^.{1}$", ErrorMessage = "Rating must be exactly 1 character long.")]
+        [Required]
         public int Rating { get; set; }
+        
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
+        [Required]
+        [StringLength(30)]
         public string Colour { get; set; }
 
     }
